22523ab9454d7c059d986a34e9604f8afc37cde2
Tyler Norbury <tylernorbury96@gmail.com>
2021-01-25 16:00:06 -0800

    Create avatar and add my name & website

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <v-row justify="center" align="center" align-content="center">
-    <v-col cols="1" align-self="center">
-      <v-avatar color="primary" size="56" class="white--text">TN</v-avatar>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="6" align-self="center">
+        <div class="text-center">
+          <v-avatar color="primary" size="128" class="white--text">
+            <v-img src="/me.jpg" alt="It's me!" />
+          </v-avatar>
+          <h2>
+            {{ name }}
+          </h2>
+          <a :href="websiteWithProtocall">
+            {{ website }}
+          </a>
+        </div>
     </v-col>
   </v-row>
-
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -21,7 +32,20 @@ export default Vue.extend({
   data() {
     return {
       name: "Tyler Norbury",
+      website: "www.tylernorbury.com",
     };
+  },
+  computed: {
+    websiteWithProtocall() {
+      return "https://" + this.$data.website;
+    },
   },
 });
 </script>
+
+<style lang="scss" scoped>
+a {
+  color: rgba(0, 0, 0, 0.75);
+  text-decoration: none;
+}
+</style>
